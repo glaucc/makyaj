9c1b35b49eb0f4d23aeb8e366bb55316f3b20f85
Puffy <88508824+PufflyMan@users.noreply.github.com>
2023-01-15 14:35:11 +0400

    Revert "some of the updates"
    This reverts commit dc316a23971af3c365b8201d2abbb41e3d873a89.

--- makyaj/src/Login.jsx
+++ makyaj/src/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
         .then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            navigate("/login")
+            navigate("/home")
             console.log(user);
         })
         .catch((error) => {
