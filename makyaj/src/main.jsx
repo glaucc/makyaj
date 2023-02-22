abcf30e2ad0f5582f9c1b3fe8ee7f29ba753cf4d
glaucc <ahmedovyusif8785@gmail.com>
2023-02-22 16:14:14 +0400

    updates

--- makyaj/src/main.jsx
+++ makyaj/src/main.jsx
@@ -5,11 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
 =======
     <RouteMain />
->>>>>>> 46872570f06fb8a0a9a896a693c62a336b987fb6
     {/* <h1 className='soon'><b><h1><a href='#'>Tezliklə...</a></h1></b></h1> */}
   </React.StrictMode>,
 )
